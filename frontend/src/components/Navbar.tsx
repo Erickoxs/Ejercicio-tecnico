e328093c64ae56744d8314b9d777c00240bf830d
Erickoxs <erickgilandrade@gmail.com>
2025-08-14 11:11:00 -0500

    Algunos Cambios

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -38,16 +38,6 @@ const Navbar: React.FC = () => {
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
           aria-label="Global"
         >
-          <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company Logo"
-              />
-            </a>
-          </div>
          
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             {userName ? (
